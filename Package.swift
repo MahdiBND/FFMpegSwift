8b25f006cc7cb23e138d8c73eba21d1616e052b4
Mahdi Bandegani <mahdi.bandegani@utechia.com>
2024-06-12 16:38:27 +0330

    add frameworks and build

--- Package.swift
+++ Package.swift
@@ -9,13 +9,46 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FFMpegSwift",
-            targets: ["FFMpegSwift"]),
+            targets: ["FFMpegSwift", "ffmpegkit", "libavcodec", "libavdevice", "libavfilter", "libavformat", "libavutil", "libswresample", "libswscale"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FFMpegSwift"),
+            name: "FFMpegSwift"
+        ),
+        .binaryTarget(
+            name: "ffmpegkit",
+            path: "ffmpeg-ios/ffmpegkit.xcframework"
+        ),
+        .binaryTarget(
+            name: "libavcodec",
+            path: "ffmpeg-ios/libavcodec.xcframework"
+        ),
+        .binaryTarget(
+            name: "libavdevice",
+            path: "ffmpeg-ios/libavdevice.xcframework"
+        ),
+        .binaryTarget(
+            name: "libavfilter",
+            path: "ffmpeg-ios/libavfilter.xcframework"
+        ),
+        .binaryTarget(
+            name: "libavformat",
+            path: "ffmpeg-ios/libavformat.xcframework"
+        ),
+        .binaryTarget(
+            name: "libavutil",
+            path: "ffmpeg-ios/libavutil.xcframework"
+        ),
+        .binaryTarget(
+            name: "libswresample",
+            path: "ffmpeg-ios/libswresample.xcframework"
+        ),
+        .binaryTarget(
+            name: "libswscale",
+            path: "ffmpeg-ios/libswscale.xcframework"
+        ),
         .testTarget(
             name: "FFMpegSwiftTests",
             dependencies: ["FFMpegSwift"]),
